0fb22594da964f9d52d408cb14f55367ad9b5f10
THRG1170 <trahmadisyawal@gmail.com>
2023-04-18 01:37:44 +0800

    deleting webpack

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{const e=document.querySelector(".dropdown"),n=document.querySelector("#dropdown-nav"),o=document.querySelector("#dropdown-nav-close");document.querySelector("ctaButton"),n.addEventListener("click",(function(){"none"==e.style.display?e.style.display="flex":e.style.display="none"})),o.addEventListener("click",(function(){e.style.display="none"}))})();
